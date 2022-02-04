43f8b146b6010ffbd6fd10a44de8e923d3857658
Jerne V <16634524+JerneV@users.noreply.github.com>
2022-02-04 12:55:38 +0100

    implemented 16b register function

---
+++ src/cpu/instructions.rs
@@ -0,0 +1,2 @@
+// This is where all instructions will come from
+// Not entirely sure how i'll do that though
--- src/cpu/registers.rs
+++ src/cpu/registers.rs
@@ -33,4 +33,37 @@ impl Registers {
         // Extract low byte and store
         self.f = (af & 0xFF) as u8;
     }
+
+    pub fn get_bc(&self) -> u16 {
+        (self.b as u16) << 8 | (self.c as u16)
+    }
+
+    pub fn set_bc(&mut self, bc: u16) {
+        // Extract high byte and shift to low byte
+        self.b = ((bc & 0xFF00) >> 8) as u8;
+        // Extract low byte and store
+        self.c = (bc & 0xFF) as u8;
+    }
+
+    pub fn get_de(&self) -> u16 {
+        (self.d as u16) << 8 | (self.e as u16)
+    }
+
+    pub fn set_de(&mut self, de: u16) {
+        // Extract high byte and shift to low byte
+        self.d = ((de & 0xFF00) >> 8) as u8;
+        // Extract low byte and store
+        self.e = (de & 0xFF) as u8;
+    }
+
+    pub fn get_hl(&self) -> u16 {
+        (self.h as u16) << 8 | (self.l as u16)
+    }
+
+    pub fn set_hl(&mut self, hl: u16) {
+        // Extract high byte and shift to low byte
+        self.h = ((hl & 0xFF00) >> 8) as u8;
+        // Extract low byte and store
+        self.l = (hl & 0xFF) as u8;
+    }
 }
