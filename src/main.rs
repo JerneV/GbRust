856f2f51d08d69d9bda5223da10ea9d8e1c30158
Jerne Vingerhoets <16634524+JerneV@users.noreply.github.com>
2022-07-02 16:26:37 +0200

    checking gpg keys

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod cpu;
 fn main() {
     let mut test_cpu = cpu::CPU::new();
     test_cpu.reg.set_af(0xFF00);
+    let _ = test_cpu.reg.get_af();
     println!("{0}", test_cpu.reg.get_af());
     println!("Hello from Main!");
 }
